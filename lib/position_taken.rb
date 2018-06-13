4e2b8ff099e18a3746e325c31218246fb6c04520
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-13 18:09:05 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    if  board[index] != " "
-    elsif board[index] != ""
-    elsif board[index] != nil
+    if  board[index] == " "
+      "false"
+    elsif board[index] == ""
+      "false"
+    elsif board[index] == nil
+      "false"
     end
 end
